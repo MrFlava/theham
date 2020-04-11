aca3ede26fed29426f3d1596290233c96cc5eef3
Mr.Flava <thatelitemaili33t@gmail.com>
2020-04-11 14:22:49 +0300

    Fixed navbar display

--- js/script.js
+++ js/script.js
@@ -2,4 +2,26 @@
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");
 });
- 
+jQuery(document).ready(function ($) {
+
+    $('.navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+    
+});
+ $(".nav .nav-link").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).addClass("active");
+});
